0d38bfcd1203f2069ddbc2358a4fe43c540793c4
Leandro Almeida <39975179+leandro-almeida98@users.noreply.github.com>
2021-10-27 09:23:37 -0300

    First Commit
    First Commit

---
+++ App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+class App extends Component{
+  render(){
+    return(
+      <View style={styles.container}>
+        <Text>Olá mundo</Text>
+      </View>
+    );
+  }
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+export default App;
